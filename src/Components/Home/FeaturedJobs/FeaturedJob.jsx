1c96007f31cfcceae4891af0cc795ca785fe0881
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-04-10 23:53:07 +0600

    some modify

--- src/Components/Home/FeaturedJobs/FeaturedJob.jsx
+++ src/Components/Home/FeaturedJobs/FeaturedJob.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const FeaturedJob = ({ job }) => {
     const navigate = useNavigate()
-    const {id, company_logo, job_title, company_name, remote_or_onsite, fulltime_or_parttime, location, salary } = job
+    const { id, company_logo, job_title, company_name, remote_or_onsite, fulltime_or_parttime, location, salary } = job
     return (
         <>
 
@@ -20,16 +20,20 @@ const FeaturedJob = ({ job }) => {
                     <button className='my-btn-secondary '>{remote_or_onsite}</button>
                     <button className='my-btn-secondary'>{fulltime_or_parttime}</button>
                 </div>
-                <div className='font-semibold flex mb-5 '>
-                    <img src="https://i.ibb.co/Y3Hc7Z7/Frame-4.png" alt="" />
-                    <p >{location}</p>
-                    <img className='ml-5 ' src="https://i.ibb.co/7vW76Nx/Frame.png" alt="" />
-                    <p>salary: {salary}</p>
+                <div className='font-semibold md:flex mb-5 '>
+                    <div className='flex mb-5 md:mb-0'>
+                        <img src="https://i.ibb.co/Y3Hc7Z7/Frame-4.png" alt="" />
+                        <p >{location}</p>
+                    </div>
+                    <div className='flex'>
+                        <img className='md:ml-5 ' src="https://i.ibb.co/7vW76Nx/Frame.png" alt="" />
+                        <p>salary: {salary}</p>
+                    </div>
                 </div>
                 <div >
-                    
-                        <button onClick={()=> navigate(`/job/${id}`)} className='my-btn'>View Details</button>
-                    
+
+                    <button onClick={() => navigate(`/job/${id}`)} className='my-btn'>View Details</button>
+
 
                 </div>
             </div>
