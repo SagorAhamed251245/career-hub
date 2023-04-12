c041d34334d2b4fe88b7a99b110df69a74228aae
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-04-12 12:07:37 +0600

    active ancor done

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
-import { Link , NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <nav className=' flex flex-col md:flex-row h-16 items-center justify-between gap-5 md:px-10'>
 
             <div className='grow font-bold md:text-3xl mt-3 md:mt-0 text-2xl'>WorkHub</div>
-            <div className='flex flex-row gap-5 font-bold  text-[#757575] '>
-            
-                <Link to='/' className='hover:text-[#7E90FE]' >
-                    Home
-                </Link>
+            <ul className='flex flex-row gap-5 font-bold  text-[#757575] '>
 
-                <Link to='/statistics' className='hover:text-[#7E90FE]'>
-                    Statistics
-                </Link>
+                <li className='hover:text-[#7E90FE]'>
+                    <NavLink to='/'  className={({isActive})=> (isActive? 'text-blue-500' : '')}  >
+                        Home
+                    </NavLink>
+                </li>
 
-                <Link to='/appliedJobs' className='hover:text-[#7E90FE]'>
-                    Applied Jobs
-                </Link>
+                <li className='hover:text-[#7E90FE]'>
+                    <NavLink to='/statistics' className={({isActive})=> (isActive? 'text-blue-500' : '')} >
+                        Statistics
+                    </NavLink>
+                </li>
 
-                <Link to='/blog' className='hover:text-[#7E90FE]'>
-                    Blog
-                </Link>
-            </div>
+                <li  className='hover:text-[#7E90FE]'>
+                    <NavLink to='/appliedJobs' className={({isActive})=> (isActive? 'text-blue-500' : '')} >
+                        Applied Jobs
+                    </NavLink>
+                </li>
+
+                <li  className='hover:text-[#7E90FE]' >
+                    <NavLink to='/blog' className={({isActive})=> (isActive? 'text-blue-500' : '')} >
+                        Blog
+                    </NavLink>
+                </li>
+            </ul>
             <div className='my-btn'>
                 <button className='title-text'>Star Applying</button>
             </div>
